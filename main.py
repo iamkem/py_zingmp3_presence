318f8df4ed93e42868b4c2b8d82627f1b5c5a4c7
badkem <hoangdinhthangloi@gmail.com>
2022-08-20 00:15:06 +0700

    presence for album

--- main.py
+++ main.py
@@ -21,18 +21,29 @@ base_url = "https://mp3.zing.vn"
 RPC.connect()
 
 
-def duration_to_mill(duration):
-    return duration * 1000
+def duration_to_mill(duration): return duration * 1000
 
 
-def song_url(key):
-    return f'{base_url}/xhr/media/get-source?type=audio&key={key}'
+def get_zing_song_data(data):
 
+    key_path = data['key_path']
 
-def get_zing_song_data(key):
-    with requests.get(song_url(key)) as req:
+    if 'video' in key_path:
+        key_path = key_path.replace('video', 'audio')
+
+    with requests.get(f'{base_url}/xhr{key_path}') as req:
         res = req.json()
-        return res['data']
+
+        song_data = {}
+        if 'album' in key_path:
+            for song in res['data']['items']:
+                if data['song_title'] == (song['name'] or song['title'] or song['album']['name'] or song['album']['title']):
+                    song_data = song
+                    break
+        else:
+            song_data = res['data']
+
+        return song_data
 
 
 def update_status(data):
@@ -40,7 +51,7 @@ def update_status(data):
         if data['playing']:
             song_data = {}
             try:
-                res = get_zing_song_data(data['key'])
+                res = get_zing_song_data(data)
                 now = int(round((time.time() - data['currentTime']) * 1000))
                 time_left = now + duration_to_mill(res['duration'])
                 song_data.update({
