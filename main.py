38103c73e1b56ba9bfcb03b90a0cbdff785c267b
badkem <hoangdinhthangloi@gmail.com>
2022-08-24 18:02:45 +0700

    new interface

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import rumps
 import asyncio
 import websockets
 import requests
@@ -13,19 +14,19 @@ client_id = 997427282606047282
 
 RPC = Presence(client_id)
 
+RPC.connect()
+
 HOST = "localhost"
 PORT = 8765
 
 base_url = "https://mp3.zing.vn"
 
-RPC.connect()
 
-
-def duration_to_mill(duration): return duration * 1000
+def duration_to_mill(duration):
+    return duration * 1000
 
 
 def get_zing_song_data(data):
-
     key_path = data['key_path']
 
     if 'video' in key_path:
@@ -37,7 +38,8 @@ def get_zing_song_data(data):
         song_data = {}
         if 'album' in key_path:
             for song in res['data']['items']:
-                if data['song_title'] == (song['name'] or song['title'] or song['album']['name'] or song['album']['title']):
+                if data['song_title'] == (
+                        song['name'] or song['title'] or song['album']['name'] or song['album']['title']):
                     song_data = song
                     break
         else:
@@ -78,7 +80,9 @@ def update_status(data):
 
 
 async def echo(websocket):
-    async for message in websocket:
+    try:
+        message = await websocket.recv()
+        print(message)
         try:
             update_status(json.loads(message))
         except json.JSONDecodeError:
@@ -87,12 +91,50 @@ async def echo(websocket):
             print('no key has found!')
         except requests.HTTPError:
             print('request error!')
+    except websockets.ConnectionClosed:
+        print('connection closed!')
 
 
-async def main():
+async def start_server():
     async with websockets.serve(echo, HOST, PORT):
+        print('server started!')
         await asyncio.Future()
 
 
+class ZingMPre(object):
+    def __init__(self):
+        self.cfg = {
+            'app_name': 'ZingMp3 Presence',
+            'sub_name': 'ZingMPre',
+            'app_icon': 'assets/logo600.png',
+            'quit_title': 'Quit ZingMPre',
+            'app_version': 'ZingMPre v1.2-beta',
+        }
+
+        self.version_button = rumps.MenuItem(self.cfg['app_version'], callback=None, icon=self.cfg['app_icon'])
+        self.connection_status_button = rumps.MenuItem('Extension - Not Connected', callback=None)
+        self.about_button = rumps.MenuItem('About', callback=self.on_about)
+        self.quit_button = rumps.MenuItem(self.cfg['quit_title'], callback=None)
+
+        self.app = rumps.App(
+            name=self.cfg['app_name'],
+            icon=self.cfg['app_icon'],
+            menu=[self.version_button, self.connection_status_button, self.about_button],
+            quit_button=self.quit_button,
+        )
+
+        self.alert_data = {
+            'title': 'About',
+            'message': 'author by @badkem',
+            'icon_path': self.cfg['app_icon'],
+        }
+
+    def on_about(self, sender):
+        rumps.alert(**self.alert_data)
+
+    def run(self):
+        self.app.run()
+
+
 if __name__ in '__main__':
-    asyncio.run(main())
+    asyncio.run(start_server())
